d6caac78f81b682945e38fec9063a59202d57d8f
kash2104 <kparikh1104@gmail.com>
2024-06-04 02:44:37 +0530

    middleware used for routes.

--- Server/routes/Event.js
+++ Server/routes/Event.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { getAllEvents } = require("../controllers/Event");
+const { auth } = require("../middleware/auth");
 
-router.get("/getAllEvents", getAllEvents);
+router.get("/getAllEvents", auth, getAllEvents);
 
 module.exports = router;
