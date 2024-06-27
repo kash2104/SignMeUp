29f9c969600398d67bd6fac5000d0084e167f68e
Tirthraj-Raval <tushartirthraj@gmail.com>
2024-06-27 18:05:32 +0530

    Conflicts and errors for both files resolved

--- Server/controllers/ErrorController.js
+++ Server/controllers/ErrorController.js
@@ -1,5 +1,5 @@
 module.exports = (err, req, res, next) => {
-    logger.error(err.stack);
+    console.error(err.stack);
     return res.status(500).json({
       success: false,
       message: "Internal Server Error",
